c122fac20caccdea5f73abff1ef00fc947695a60
rkram5424 <rkram5424@gmail.com>
2014-06-25 00:09:50 -0500

    Added flags to scrollview. Made step text text boxes to solce wrapping problem. 'Michael is a poop head' -> 'Michael makes sense.'

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from sys import platform as _platform
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
 from kivy.properties import ListProperty
 from kivy.core.window import Window
 import Babelrang
 
 class MainScreen(Screen):
 	steps = ListProperty()
-	def twist_button(self, in_message):
+	def throw_button(self, in_message):
+		self.ids.result_grid.clear_widgets()
 		br = Babelrang.Babelrang(in_message)
 		self.ids.out_message.text = br.get_output()[1]
 		self.steps = br.get_output()[0]
+		print self.steps
+		for index in range(len(self.steps)):
+			line_box = BoxLayout()
+			line_box.add_widget(Image(source = 'Art/Flags/' + self.steps[index][1] + '.png', size_hint_x = 0.1))
+			step_text = TextInput(text = self.steps[index][0], readonly = True)
+			line_box.add_widget(step_text)
+			self.ids.result_grid.add_widget(line_box)
 		print self.steps
 
 Builder.load_string("""
@@ -34,13 +45,13 @@ Builder.load_string("""
 			BoxLayout:
 				TextInput:
 					id: in_message
-					hint_text: 'Input'
+					hint_text: 'Message to Translate. 250 characters max.'
 					multiline: True
 				Button:
-					id: twist_button
+					id: throw_button
 					size_hint_x: 0.2
 					text: 'Throw'
-					on_press: root.twist_button(in_message.text)
+					on_press: root.throw_button(in_message.text)
 			BoxLayout:
 				size_hint_y: 7
 				canvas:
@@ -49,9 +60,15 @@ Builder.load_string("""
 					Rectangle:
 						pos: self.pos
 						size: self.size
-				ListView:
-					id: steps
-					item_strings: [root.steps[index][0] for index in range(len(root.steps))]
+				ScrollView:
+					GridLayout:
+						padding: 10
+						spacing: 10
+                		size_hint_y: 4
+                		width: 500
+						id: result_grid
+						cols: 1
+						rows: 20
 			TextInput:
 				id: out_message
 				hint_text: 'Result'
